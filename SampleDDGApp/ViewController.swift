dcc48cbec35ad125e8b3115006d04209266e5cfd
Lone Leamer <leamerluck@gmail.com>
2015-10-19 20:16:58 -0400

    Initial Commit

---
+++ SampleDDGApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SampleDDGApp
+//
+//  Created by Christina McIntyre on 10/19/15.
+//  Copyright © 2015 Christina McIntyre. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
